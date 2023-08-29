4029ed6366758d5fbd0e7be80c8c775e8c15f78d
Adeel Ahmed <adeelahmed992@gmail.com>
2023-08-29 11:49:20 +0500

    config1

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'teams' );
+define( 'DB_NAME', 'teams_' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
