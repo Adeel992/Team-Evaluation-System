d8f8d671f14bfc6bf6c0d72c04f281cb7cf6f3b7
Adeel Ahmed <adeelahmed992@gmail.com>
2023-08-29 11:44:28 +0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'teams' );
+define( 'DB_NAME', 'db_name' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'db_user' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'password_here' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
